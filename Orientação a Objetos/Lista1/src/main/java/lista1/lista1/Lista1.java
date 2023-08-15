f7ca1c32d8c087b5d3f65714167a5259ce504814
GhostBlkz <111324297+GhostBlkz@users.noreply.github.com>
2023-08-15 09:10:19 -0300

    Add files via upload

--- Orientação a Objetos/Lista1/src/main/java/lista1/lista1/Lista1.java
+++ Orientação a Objetos/Lista1/src/main/java/lista1/lista1/Lista1.java
@@ -3,31 +3,21 @@ public class Lista1 {
 
     public static void main(String[] args) {
         
-        Produto obj1 = new Produto();
-        obj1.codigo = 1;
-        obj1.nome = "Sabonete";
-        obj1.descricao = "Dove Creme";
-        obj1.quantidade = 10;
-        obj1.preco = 1.8f;
+        Produto obj1 = new Produto(1, "sabonete", "Dove Creme", 10, 1.8f);
         
-        Produto obj2 = new Produto();
-        obj2.codigo = 2;
-        obj2.nome = "Sabão";
-        obj2.descricao = "Neutro";
-        obj2.quantidade = 20;
-        obj2.preco = 1.2f;
+        Produto obj2 = new Produto(2, "sabão", "neutro", 20, 1.2f);
         
         //exibe dados do obj1
-        obj1.exibeDados();
-        //exibe dados do obj2
-        obj2.exibeDados();
+ 
         
          obj1.comprar(5);
          obj2.comprar(8);
          obj1.vender(16);
-        //exibe dados do obj1
+        
+        obj1.DiminuirPreco(3.0f);
+        obj2.aumentarPreco(5.0f);
+        
         obj1.exibeDados();
-        //exibe dados do obj2
         obj2.exibeDados(); 
                 
     }
--- Orientação a Objetos/Lista1/src/main/java/lista1/lista1/Produto.java
+++ Orientação a Objetos/Lista1/src/main/java/lista1/lista1/Produto.java
@@ -8,6 +8,18 @@ public class Produto {
     public String descricao;
     public int quantidade;
     public float preco;
+    
+    //metodo construtor, tem o mesmo nome da classe
+    public Produto (int codigo, String nome, String descricao, int quantidade, float preco){
+        //this codigo representa o codigo do obj que chama o construtor 
+        //codigo representa o valor informado pelo usuario
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+        this .quantidade = quantidade;
+        this.preco = preco; 
+  
+    }
     //compra produtos
     public void comprar(int x){
         this.quantidade = this.quantidade + x;
@@ -23,5 +35,17 @@ public class Produto {
                System.out.println("Codigo: "+this.codigo+ "\nNome: "+this.nome+ "\nDescrição: "+this.descricao+"\nquantidade: "
                 +this.quantidade+"\nPreço: "+ this.preco);
               
+    }
+    //aumentar o preco do produto
+    public void aumentarPreco(float x){
+        this.preco = this.preco + x; 
+    }
+    //diminuir preco do produto
+    public void DiminuirPreco(float x){
+        if (x > this.preco){
+            System.out.println("O preco nao pode ser negativo");}
+        else {
+            this.preco = this.preco - x;
+        } 
     }
 }
